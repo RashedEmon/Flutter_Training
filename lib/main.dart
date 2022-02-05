8a1d3809a1e0c62a9682fb1a15c52a50b7d86b9d
Rashed <rashedulb13@gmail.com>
2022-02-05 21:13:36 +0600

    Layout Practice bacth-58

--- lib/main.dart
+++ lib/main.dart
@@ -1,210 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// void main() {
-//   runApp(const MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-//
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-//
-//   final String title;
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           leading: const Icon(Icons.menu_open_sharp),
-//           actions: const [
-//             Icon(Icons.lock),
-//           ],
-//         ),
-//         body: Column(
-//           children: [
-//             Container(
-//               alignment: Alignment.topLeft,
-//               width: MediaQuery.of(context).size.width,
-//               padding: const EdgeInsets.fromLTRB(10, 50, 5, 10),
-//               child: Column(
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: const [
-//                   Text(
-//                     'Eat Healthy',
-//                     style: TextStyle(
-//                       fontSize: 35,
-//                     ),
-//                   ),
-//                   Text(
-//                     'Our Daily Healthy Food Habit',
-//                     style: TextStyle(
-//                       color: Colors.grey,
-//                     ),
-//                   )
-//                 ],
-//               ),
-//             ),
-//             SingleChildScrollView(
-//               scrollDirection: Axis.horizontal,
-//               child: Row(
-//                 mainAxisAlignment: MainAxisAlignment.start,
-//                 children: [
-//                   Container(
-//                     padding:
-//                     const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//                     decoration: BoxDecoration(
-//                         borderRadius:
-//                         const BorderRadius.all(Radius.circular(10)),
-//                         border: Border.all(color: Colors.grey, width: 1)),
-//                     child: const Text('Fruits'),
-//                   ),
-//                   Container(
-//                     padding:
-//                     const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//                     decoration: BoxDecoration(
-//                         borderRadius:
-//                         const BorderRadius.all(Radius.circular(10)),
-//                         border: Border.all(color: Colors.grey, width: 1)),
-//                     child: const Text('Fruits'),
-//                   ),
-//                   Container(
-//                     padding:
-//                     const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//                     decoration: BoxDecoration(
-//                         borderRadius:
-//                         const BorderRadius.all(Radius.circular(10)),
-//                         border: Border.all(color: Colors.grey, width: 1)),
-//                     child: const Text('Fruits'),
-//                   ),
-//                   Container(
-//                     padding:
-//                     const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//                     decoration: BoxDecoration(
-//                         borderRadius:
-//                         const BorderRadius.all(Radius.circular(10)),
-//                         border: Border.all(color: Colors.grey, width: 1)),
-//                     child: const Text('Vegetables'),
-//                   ),
-//                   Container(
-//                     padding:
-//                     const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-//                     decoration: BoxDecoration(
-//                         borderRadius:
-//                         const BorderRadius.all(Radius.circular(10)),
-//                         border: Border.all(color: Colors.grey, width: 1)),
-//                     child: const Text('Grilled'),
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Padding(
-//               padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-//               child: SingleChildScrollView(
-//                 scrollDirection: Axis.vertical,
-//                 child: Row(
-//                   crossAxisAlignment: CrossAxisAlignment.start,
-//                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                   children: [
-//                     Column(
-//                       children: [
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.amber,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.black,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.black,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.black,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.black,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                       ],
-//                     ),
-//                     const SizedBox(
-//                       width: 10,
-//                     ),
-//                     Column(
-//                       children: [
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.redAccent,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.blueAccent,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.blueAccent,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.blueAccent,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                         Container(
-//                           margin: const EdgeInsets.only(top: 10),
-//                           color: Colors.blueAccent,
-//                           width: MediaQuery.of(context).size.width / 2.2,
-//                           height: 120,
-//                         ),
-//                       ],
-//                     )
-//                   ],
-//                 ),
-//               ),
-//             )
-//           ],
-//         )
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -222,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -243,150 +37,62 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.arrow_back),
-          centerTitle: true,
-          title: const Text(
-            'Start Activity',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-        ),
         body: Container(
-          margin: EdgeInsets.symmetric(horizontal: (MediaQuery.of(context).size.width/100)*2,vertical: (MediaQuery.of(context).size.width/100)*1),
+          margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 20),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*46,
-                        height: (MediaQuery.of(context).size.width/100)*45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.motorcycle),
-                            Text('Cycling')
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 5,),
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*46,
-                        height: (MediaQuery.of(context).size.width/100)*45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.map),
-                            Text('Travel')
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 5,),
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*46,
-                        height: (MediaQuery.of(context).size.width/100)*45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.music_video),
-                            Text('Shooting')
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  //const SizedBox(width: 70,),
-                  Column(
-                    children: [
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*46,
-                        height: (MediaQuery.of(context).size.width/100)*45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.car_rental),
-                            Text('Rent Car')
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 5,),
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*46,
-                        height: (MediaQuery.of(context).size.width/100)*45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.car_rental),
-                            Text('Rent Car')
-                          ],
-                        ),
-                      ),
-                      const SizedBox(height: 5,),
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*46,
-                        height: (MediaQuery.of(context).size.width/100)*45,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: const [
-                            Icon(Icons.car_rental),
-                            Text('Rent Car')
-                          ],
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Row(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*15,
-                        height: (MediaQuery.of(context).size.width/100)*10,
-                      ),
-
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*15,
-                        height: (MediaQuery.of(context).size.width/100)*10,
-                      ),
-
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*15,
-                        height: (MediaQuery.of(context).size.width/100)*10,
-                      ),
-
-                      Container(
-                        color: Colors.amber,
-                        width: (MediaQuery.of(context).size.width/100)*15,
-                        height: (MediaQuery.of(context).size.width/100)*10,
-                      )
-                    ],
-                  ),
-                  ]
-                ),
+              customeCard(title:"Praktikum PCD", subTitle: "Tahun ajaran 2019/2020", username: "Puja Hanifah", mainColor: Colors.red, secondaryColor: Colors.redAccent),
+              customeCard(title: "Bengkel GIS", subTitle: "Tahun Ajaran 2019/2020", username: "Iqbal Mahtama",mainColor: Colors.blue, secondaryColor: Colors.blueAccent),
+              customeCard(title: "Animasu Computer", subTitle: "Tahun Ajaran 2019/2020", username: "Aida kamila")
             ],
-
           ),
         )
+    );
+  }
+
+  customeCard({required String title,required String subTitle, required String username, Color mainColor=Colors.green,Color secondaryColor=Colors.lightGreen}){
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.width/3,
+      color: mainColor,
+      child: Container(
+        margin: const EdgeInsets.only(left: 20),
+        padding: const EdgeInsets.only(left: 20,top: 10,right: 20,bottom: 15),
+        color: secondaryColor,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 25
+                  ),),
+                Container(
+                    padding: const EdgeInsets.all(3),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white
+                    ),
+                    child: const Icon(Icons.notifications)
+                ),
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).size.width/100,),
+            Text(subTitle),
+            SizedBox(height: (MediaQuery.of(context).size.width/100)*5,),
+            Row(
+              children: [
+                const Icon(Icons.person),
+                Text(username)
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
